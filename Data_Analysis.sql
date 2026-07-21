10d5c8f1f6709c4a8e98aee0667ab1b65a38ca7c
Ozge Guney <ozgeeozr@gmail.com>
2026-07-21 14:50:33 +0100

    Update Data_Analysis.sql

--- Data_Analysis.sql
+++ Data_Analysis.sql
@@ -2,24 +2,27 @@
 
 -- Joins --
 
+
 -- Shows the books that have been borrowed
 SELECT borrowed_books.Book_ID, books.title, borrowed_books.borrow_date, borrowed_books.due_date, borrowed_books.return_date, borrowed_books.Member_ID
 FROM borrowed_books
 INNER JOIN books ON borrowed_books.Book_ID = books.Book_ID;
 
 -- Shows the authors together with their books
+
 SELECT authors.author_ID, authors.first_Name, authors.last_name, books.title, books.book_id
 FROM authors
 INNER JOIN books ON authors.author_ID = books.author_ID;
 
-/*
 -- Find Books by Genre
+
 SELECT books.Book_ID,books.Title, Genres.Genre
 FROM books
 JOIN Genres ON books.Genre_ID = Genres.GenreID
 WHERE Genres.Genre = 'philosophy';
 
 -- Calculate late returns
+
 SELECT librarymembers.FirstName, librarymembers.LastName, books.Title, borrowed_books.Due_date, borrowed_books.Return_date,
        DATEDIFF(borrowed_books.Return_date, borrowed_books.Due_date) AS OverdueDays
 FROM librarymembers
@@ -45,7 +48,8 @@ SET @book_1_duration = CalculateBorrowDuration('2023-12-01','2023-12-07');
 SELECT @book_1_duration;
 
 -- Update Available Copies
-/* DELIMITER //
+
+DELIMITER //
 CREATE FUNCTION UpdateAvailableCopies(bookID INT)
 RETURNS INT
 DETERMINISTIC
@@ -60,11 +64,10 @@ BEGIN
     WHERE Book_ID = bookID AND Return_date IS NULL; -- Calculate and return the available copies
     RETURN totalCopies - borrowedCopies;
 END //
-DELIMITER ; */
-
--- Example queries --
+DELIMITER ;
 
 -- Query with subquery: transactions of fiction books
+
 SELECT bb.Transaction_ID, bb.Book_ID, bb.Member_ID
 FROM borrowed_books AS bb
 WHERE Book_ID IN (
@@ -73,12 +76,8 @@ WHERE Book_ID IN (
     WHERE Genre_ID BETWEEN 1 and 2
     );
     
+-- Stored procedures: Log a Book being Borrowed 
 
-/* ADVANCED OPTIONS */
-
--- Stored procedures --
-
--- Stored Procedure 1 (SP1): Log a Book being Borrowed 
 DELIMITER // 
 CREATE PROCEDURE LogBookBorrowed( 
 IN p_BookID INT, 
@@ -104,13 +103,15 @@ END //
 DELIMITER ;
 
 -- Call the LogBookBorrowed stored procedure to log new borrowed book:
+
 CALL LogBookBorrowed(20, '2016912376', 'CN2221');
 -- Could have a trigger to check there are available copies before borrowing
 -- DROP PROCEDURE LogBookBorrowed;
 
 -- Triggers --
 
-/* -- Update Available Copies upon Borrowing
+-- Update Available Copies upon Borrowing
+
 DELIMITER //
 CREATE TRIGGER AfterBorrowedBooksInsert
 AFTER INSERT ON borrowed_books
@@ -134,9 +135,10 @@ BEGIN
     SET Available_copies = updatedAvailableCopies
     WHERE Book_ID = borrowed_books.Book_ID;
 END //
-DELIMITER ; */
+DELIMITER ;
 
 -- Trigger for staff table to generate staff ID
+
 DELIMITER $$
 CREATE TRIGGER before_staff_insert
 BEFORE INSERT ON staffmembers
@@ -159,20 +161,22 @@ END $$
 DELIMITER ;
 
 -- Code to call trigger
+
 INSERT INTO staffmembers (Title, FirstName, LastName, DateOfBirth, ContactPhoneNumber, Department, SalaryBand, HireDate) VALUES
 ('Mr.', 'James', 'Doe', '1990-01-15', '07123 456789', 'Reference', '7-30', '2023-12-07');
 -- DELETE FROM staffmembers WHERE FirstName = 'James' AND LastName = 'Doe';
 
 -- Events -- 
 
-/* -- Create an event to log a book being borrowed every day
+-- Create an event to log a book being borrowed every day
+
 CREATE EVENT IF NOT EXISTS daily_log_book_borrowed_event
 ON SCHEDULE EVERY 1 DAY
 DO
 BEGIN
     CALL LogBookBorrowed(20, '2016912376', 'CN2221');
     -- You can adjust the parameters or add more logic as needed
-END; */
+END;
 
 -- Views --
 
@@ -188,12 +192,15 @@ WHERE g.Genre = 'philosophy'
 GROUP BY first_name
 ORDER BY Title;
 
-/* -- View philosophy books in the library
+-- View philosophy books in the library
+
 CREATE OR REPLACE VIEW philosophybooks AS
 SELECT genres.genre, books.title, authors.first_name, authors.last_name
 FROM genres, books, authors
-WHERE genres.genre = 'philosophy'; -- doesn't work properly
+WHERE genres.genre = 'philosophy'; 
+
 -- View books by Amyrata Sen
+
 CREATE OR REPLACE VIEW books_by_Sen AS
 SELECT books.author_ID, books.title
 FROM books
